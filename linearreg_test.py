334ddd83afc14276b76347091b900df449b62841
Sidney Goehler <crazy_gosu_sidney@hotmail.de>
2020-04-27 11:50:29 +0200

    commit

--- linearreg_test.py
+++ linearreg_test.py
@@ -5,8 +5,12 @@ import linearreg as lreg
 
 if __name__ == "__main__":
 
-    x = np.matrix([[1, 2], [4, 5]])
+    x = np.matrix([[1, 2, 4], [4, 5, 7],[5, 8, 9],[7,8,9]])
     print(x,x.shape)
     x1 = lreg.extend_matrix(x)
 
     print(x1,x1.shape)
+
+    x2 = lreg.extend_matrix(x1)
+
+    print(x2,x2.shape)
---
+++ uebungsblatt.py
@@ -0,0 +1,17 @@
+import math
+
+
+if __name__ == "__main__":
+
+    a = math.sin(60)
+
+    b = math.atan(1)
+
+    c = math.e**2
+
+    d = math.log2(1+4.3e-4)
+
+    print("Aufgabe 1: a = {}, b = {}, c = {}, d = {} ".format(a,b,c,d))
+
+    tempC = 20
+    print(type(tempC))
